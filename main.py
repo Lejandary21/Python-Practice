6a605c82a73aeb54336ce9e4013dd712c959c462
Lejandary21 <159848652+Lejandary21@users.noreply.github.com>
2024-05-02 12:28:28 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Ask the user their name
+name = input("What's your name?")
+# Greet the user and introduce the quiz
+print("Welcome to the quiz,",name)
+print("this quiz is about capital cities of the world.")
+# Ask the user a question
+answer =input("What's New Zealand's capital city?")
+# Tell the user the answer
+if answer == "Wellington" :
+    print("Correct! It is indeed WellingtonLeja")
+# End the quiz
+print("Thank you for taking the quiz!")
