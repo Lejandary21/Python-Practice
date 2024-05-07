34f8f6e7b8f4593c4aef2a0d036a7bbc71515cd5
Lejandary21 <159848652+Lejandary21@users.noreply.github.com>
2024-05-07 12:29:04 +1200

    V.3

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ name = input("What's your name?")
 print("Hello! Welcome to the quiz" ,name)
 print("this quiz is about capital cities of the world.")
 #Question 1
-answer =input("What's New Zealand's capital city?")
-if answer == "Wellington" :
+question = "What is the capital of New Zealand?"
+a = "Wellington"
+b = "New York"
+c = "London"
+d = "Rio De Janeiro"
+answer  = input("{})\nA.{} B.{} C. {} D. {}".format(question, a, b, c, d)).lower()
+
+score += 5
+if answer.lower() == a or answer.lower() == "a":
     print("Correct! It is indeed Wellington")
-    score += 5
-elif answer == "":
-    print("Cat got your tongue?")
 else: 
     print("Sorry! That's incorrect. :(")
     print("The correct answer is Wellington! Better luck next time.")
 #Question 2
-answer =input("What's Australia's capital city?")
-if answer == "Canberra" :
+answer =input("What's Australia's capital city?").upper()
+if answer == "CaNbErRa".upper():
     print("Correct! It is indeed Canberra. Good job!")
     score += 5
 elif answer == "":
@@ -24,8 +28,8 @@ else:
     print("Sorry! That's incorrect. :(")
     print("The correct answer is Canberra! Better luck next time.")
 #Question 3
-answer =input("What's Japan's capital city?")
-if answer == "Tokyo" :
+answer =input("What's Japan's capital city?").upper()
+if answer == "ToKyO".upper():
     print("Correct! It is indeed Tokyo!")
     score +=5
 elif answer == "":
@@ -34,5 +38,4 @@ else:
     print("Sorry! That's incorrect. :(")
     print("The correct answer is Tokyo! Better luck next time.")
 # Ending
-print("Thank you for taking the quiz!")
-print("Your final score is" ,score)
+print("Good job {}! Thank you so much for answering my quiz! Your final score {} out of 15".format(name, score)) 
